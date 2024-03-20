a90f02fa6e3b6da4d1fdf210bac68400f66d7c09
Olivier DEBAUCHE <olivier.debauche@umons.ac.be>
2024-03-20 22:47:05 +0100

    Test

---
+++ Docker/main.py
@@ -0,0 +1,20 @@
+from openai import OpenAI
+import json
+
+with open('Docker/cuda/config-cuda.json', 'r') as file:
+    config = json.load(file)
+
+client = OpenAI(base_url="http://localhost:8008/v1", api_key="")
+for model_config in config['models']:
+    response = client.chat.completions.create(
+        model=model_config['model_alias'],
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": "Who are the American presidents?"},
+                ],
+            }
+        ],
+    )
+    print(response)
