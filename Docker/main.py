a457a669dd65775e9d475b5262751d3b0c5dea4f
Olivier DEBAUCHE <olivier.debauche@umons.ac.be>
2024-03-20 22:52:18 +0100

    Test

--- Docker/main.py
+++ Docker/main.py
@@ -6,6 +6,7 @@ with open('Docker/cuda/config-cuda.json', 'r') as file:
 
 client = OpenAI(base_url="http://localhost:8008/v1", api_key="")
 for model_config in config['models']:
+    print(model_config['model_alias'])
     response = client.chat.completions.create(
         model=model_config['model_alias'],
         messages=[
